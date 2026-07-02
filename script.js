7429cc18bd2e7729eb079d5c1b23549c212ab053
Prabhakar Shukla <prabhakarshukla669@gmail.com>
2026-07-02 17:09:30 +0530

    scrolling effect added

---
+++ script.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
