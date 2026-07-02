888682ad556684010db8afb3d4ae296dda1aa2df
Prabhakar Shukla <prabhakarshukla669@gmail.com>
2026-07-02 17:35:31 +0530

    added hambburger feature for responsiveness

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ window.addEventListener("scroll", function () {
     }
 
 });
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
